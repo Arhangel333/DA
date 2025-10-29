0ee50650d68ffec5afe0c4b9ecede5ec454873f6
White_Arhangel <maks.kurnosov@bk.ru>
2025-10-29 03:02:13 +0300

    лаба 7

--- 2025.2/7lab/main.cpp
+++ 2025.2/7lab/main.cpp
@@ -14,6 +14,26 @@ int main() {
     for (size_t i = 0; i < n; i++) {
         vector<int64_t> row(n, 0);
         poli_count[i] = row;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+        poli_count[i][i] = 1;
+    }
+
+    for (size_t l = 1; l <= n; l++) {
+        int i = 0, j;
+        while(i + l < n) {  
+            j = i + l;      
+            if(s[i] == s[j]) {            
+                poli_count[i][j] = poli_count[i + 1][j] + poli_count[i][j - 1] + 1;
+            }
+            else {                        
+                poli_count[i][j] = poli_count[i + 1][j] + poli_count[i][j - 1] - poli_count[i + 1][j - 1];
+            }
+            i++;
+            
+=======
+>>>>>>> ec0f23e (лаба 7)
     }
 
     for (size_t i = 0; i < n; i++) {
@@ -34,6 +54,10 @@ int main() {
                 poli_count[i][i + l] = poli_count[i + 1][i + l] + poli_count[i][i + l - 1] - poli_count[i + 1][i + l - 1];
             }
             i++;
+<<<<<<< HEAD
+=======
+>>>>>>> 8d890ca5ca283e30814d936e28ecd649c5e3d7c2
+>>>>>>> ec0f23e (лаба 7)
         }
     }
     
